6e39633847baaf17582114487b74a4c5cd03eb1f
laihipp <71467721+laihipp@users.noreply.github.com>
2021-10-28 20:25:57 -0500

    Update Program2.jl
    format report tables

--- Program2.jl
+++ Program2.jl
@@ -1,4 +1,15 @@
+# Written by Amber Lai Hipp and Ian McNichols
+# File Name: Project2.jl
+# Programming Assignment 2
+# CS 424-01
+# October 28, 2021
+# Tested on macOS 11.6 using VSCode
+# This program reads baseball player information from a given file, calculates statistics, creates a list of players,
+# sorts the players by the on-base plus slugging statistic, and prints a report to the screen. It then sorts the players
+# by batting average and prints a second report to the screen.
+
 using Base: slug
+using Printf
 println("Starting new program\n")
 # Declare variable lists
 names = []
@@ -18,7 +29,7 @@ OPS = []
 # Will hold sublists of each player with their calculated statistics
 outputs = []
 # Read in players from file line-by-line
-player_data = readlines("C://Users//ian//Desktop//Courses//CS 424//batters.txt");
+player_data = readlines("playerInfo.txt");
 # Add values from file to lists
 for player in player_data
     # Split the player information and add it to each of the variable lists
@@ -37,38 +48,42 @@ end
 # Initialize statistics arrays
 for i in 1:length(player_data)
     # Calculate the statistics of the current batter and add it to the outputs
-    push!(averages, round((singles[i] + doubles[i] + triples[i] + homeruns[i]) / at_bats[i], digits=3))
-    push!(slugging_percent, round((singles[i] + 2*doubles[i] + 3*triples[i] + 4*homeruns[i]) / at_bats[i], digits=3))
-    push!(OBP, round((hitByPitch[i] + walks[i] + singles[i] + doubles[i] + triples[i] + homeruns[i]) / plate_appearances[i], digits=3))
-    push!(OPS, round(slugging_percent[i] + OBP[i], digits= 3))
+    push!(averages, (singles[i] + doubles[i] + triples[i] + homeruns[i]) / at_bats[i])
+    push!(slugging_percent, (singles[i] + 2*doubles[i] + 3*triples[i] + 4*homeruns[i]) / at_bats[i])
+    push!(OBP, (hitByPitch[i] + walks[i] + singles[i] + doubles[i] + triples[i] + homeruns[i]) / plate_appearances[i])
+    push!(OPS, (slugging_percent[i] + OBP[i]))
 end
 
-# Print statistics
+# # Print statistics
 for i in 1:length(player_data)
-    push!(outputs, [names[i][2], names[i][1],  averages[i], slugging_percent[i], OBP[i], OPS[i]])
-    #println(names[i][2], ", ", names[i][1],  " ", averages[i], " ", slugging_percent[i], " ", OBP[i], " ", OPS[i])
+    push!(outputs, [names[i][2], names[i][1], averages[i], slugging_percent[i], OBP[i], OPS[i]])
+
 end
 
-println("BASEBALL TEAM REPORT --- ", length(player_data), " PLAYERS FOUND IN FILE")
-println("   PLAYER NAME     :   AVERAGE     SLUGGING    ONBASE%     OPS")
-println("--------------------------------------------------------------------")
+println("Baseball Team Summary:     ", length(player_data), " players found in file.\n\n")
+
+println("Players Sorted by OPS:\n\n")
+println("     PLAYER NAME     :     AVERAGE  SLUGGING  ONBASE%   OPS")
+println("------------------------------------------------------------")
 # Sort by OPS
 sort!(outputs, lt=(x,y)->isless(y[6], x[6]))
-for i in 1:length(player_data)
-    for j in 1:length(outputs)
-        print(outputs[i][j], " ")
-    end
+for i in 1:length(outputs)
+    tempName = outputs[i][1] * ", " * outputs[i][2]
+    @printf("%20s : %10.3f %8.3f %8.3f %8.3f\n", tempName, outputs[i][3], outputs[i][4], outputs[i][5], outputs[i][6])
     print("\n")
 end
-println("   PLAYER NAME     :   AVERAGE     SLUGGING    ONBASE%     OPS")
-println("--------------------------------------------------------------------")
+println("------------------------------------------------------------")
+
+
+println("\n\nPlayers Sorted by Batting Average:\n\n")
+println("     PLAYER NAME     :     AVERAGE  SLUGGING  ONBASE%   OPS")
+println("------------------------------------------------------------")
 # Sort by Batting averages
 sort!(outputs, lt=(x,y)->isless(y[3], x[3]))
-for i in 1:length(player_data)
-    for j in 1:length(outputs)
-        print(outputs[i][j], " ")
-    end
+for i in 1:length(outputs)
+    tempName = outputs[i][1] * ", " * outputs[i][2]
+    @printf("%20s : %10.3f %8.3f %8.3f %8.3f\n", tempName, outputs[i][3], outputs[i][4], outputs[i][5], outputs[i][6])
     print("\n")
 end
-
+println("------------------------------------------------------------")
 println("\nProgram ended.")
