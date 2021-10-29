a3ecdd41afc3bf9a972a501e07728cb9c8f6f578
laihipp <71467721+laihipp@users.noreply.github.com>
2021-10-28 22:07:34 -0500

    Update Program2.jl
    I think it's done!

--- Program2.jl
+++ Program2.jl
@@ -8,42 +8,28 @@
 # sorts the players by the on-base plus slugging statistic, and prints a report to the screen. It then sorts the players
 # by batting average and prints a second report to the screen.
 
-using Base: slug
+using Base:slug
 using Printf
 
-
-function print_teams(outputs)
-    # Print out the team statistics by OPS and Batting Average
-    println("Baseball Team Summary:     ", length(outputs), " players found in file.\n\n")
-
-    println("Players Sorted by OPS:\n\n")
+# Print out the team report sorted by value/category passed in
+function print_team_report(player_outputs, sort_value, sort_category)
+    # Print header info
+    println("\n\nPlayers Sorted by ", sort_category, ":\n\n")
     println("     PLAYER NAME     :     AVERAGE  SLUGGING  ONBASE%   OPS")
     println("------------------------------------------------------------")
-    # Sort by OPS
-    sort!(outputs, lt=(x,y)->isless(y[6], x[6]))
-    for i in 1:length(outputs)
-        tempName = outputs[i][1] * ", " * outputs[i][2]
-        @printf("%20s : %10.3f %8.3f %8.3f %8.3f\n", tempName, outputs[i][3], outputs[i][4], outputs[i][5], outputs[i][6])
-        print("\n")
-    end
-    println("------------------------------------------------------------")
-    println("\n\nPlayers Sorted by Batting Average:\n\n")
-    println("     PLAYER NAME     :     AVERAGE  SLUGGING  ONBASE%   OPS")
-    println("------------------------------------------------------------")
-    # Sort by Batting averages
-    sort!(outputs, lt=(x,y)->isless(y[3], x[3]))
-    for i in 1:length(outputs)
-        tempName = outputs[i][1] * ", " * outputs[i][2]
-        @printf("%20s : %10.3f %8.3f %8.3f %8.3f\n", tempName, outputs[i][3], outputs[i][4], outputs[i][5], outputs[i][6])
+    # Sort by desired value
+    sort!(player_outputs, lt=(x, y) -> isless(y[sort_value], x[sort_value]))
+    # Print sorted list
+    for i in 1:length(player_outputs)
+        tempName = player_outputs[i][1] * ", " * player_outputs[i][2]
+        @printf("%20s : %10.3f %8.3f %8.3f %8.3f\n", tempName, player_outputs[i][3], player_outputs[i][4], player_outputs[i][5], player_outputs[i][6])
         print("\n")
     end
     println("------------------------------------------------------------")
 end
 
-
-function read_file(filename)
-    # Read in the player file and calculate statistics from variables
-
+# Get filename from user, read file, and calculate statistics from variables.
+function read_file()
     # Declare variable lists
     names = []
     plate_appearances = []
@@ -60,9 +46,24 @@ function read_file(filename)
     OBP = []
     OPS = []
     # Will hold sublists of each player with their calculated statistics
-    outputs = []
+    player_outputs = []
+
+    # Get filename from user
+    print("\nEnter the name of your input file: ")
+    myfile = nothing
+    filename = readline()
+
     # Read in players from file line-by-line
-    player_data = readlines(filename);
+    try
+        myfile = open(filename)
+
+    catch err
+        println("\nUnable to open the file: $filename")
+	    println("Exiting the program\n")
+	    exit(0)
+    end
+    player_data = readlines(myfile);
+
     # Add values from file to lists
     for player in player_data
         # Split the player information and add it to each of the variable lists
@@ -77,28 +78,44 @@ function read_file(filename)
         push!(walks, parse(Float64, player[9]))
         push!(hitByPitch, parse(Float64, player[10]))
     end
+
     # Initialize statistics arrays
     for i in 1:length(player_data)
-        # Calculate the statistics of the current batter and add it to the outputs
+        # Calculate the statistics of the current player
         push!(averages, (singles[i] + doubles[i] + triples[i] + homeruns[i]) / at_bats[i])
-        push!(slugging_percent, (singles[i] + 2*doubles[i] + 3*triples[i] + 4*homeruns[i]) / at_bats[i])
+        push!(slugging_percent, (singles[i] + 2 * doubles[i] + 3 * triples[i] + 4 * homeruns[i]) / at_bats[i])
         push!(OBP, (hitByPitch[i] + walks[i] + singles[i] + doubles[i] + triples[i] + homeruns[i]) / plate_appearances[i])
         push!(OPS, (slugging_percent[i] + OBP[i]))
     end
-    # Initialize the output statistics array
+
+    # Initialize player_outputs array
     for i in 1:length(player_data)
-        push!(outputs, [names[i][2], names[i][1], averages[i], slugging_percent[i], OBP[i], OPS[i]])
+        push!(player_outputs, [names[i][2], names[i][1], averages[i], slugging_percent[i], OBP[i], OPS[i]])
     end
     
-return outputs
+    # Print team summary
+    println("\n\nBaseball Team Summary:     ", length(player_outputs), " players found in file.")
+    return player_outputs
 end
 
 
 function main()
     # Driver function to kick off calculations
-    println("Starting new program\n")
-    outputs = read_file("C://Users//ian//Desktop//Courses//CS 424//playerInfo.txt")
-    print_teams(outputs)
+    println("Starting new program...\n")
+
+    # Print welcome statement
+    println("\nWelcome! This program will read baseball player information from a given file,\n" *
+		"calculate the averages for each player, and then print out two reports.")
+    
+    # Read file and get list of players and statistics
+    player_outputs = read_file()
+
+    # Print report sorted by OPS
+    print_team_report(player_outputs, 6, "OPS")
+
+    # Print Report sorted by batting average
+    print_team_report(player_outputs, 3, "Batting Average")
+
     println("\nProgram ended.")
 end
 
